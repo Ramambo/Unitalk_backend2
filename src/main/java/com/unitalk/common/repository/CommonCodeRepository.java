34782355010b2f5d83bc025e2c82fd6e1cd3cdc8
JinyDev <wonjiny.dev@gmail.com>
2024-06-28 00:57:39 +0900

    feat: create program entity v3

---
+++ src/main/java/com/unitalk/common/repository/CommonCodeRepository.java
@@ -0,0 +1,7 @@
+package com.unitalk.common.repository;
+
+import com.unitalk.common.model.entity.CommonCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonCodeRepository extends JpaRepository<CommonCode, String> {
+}
