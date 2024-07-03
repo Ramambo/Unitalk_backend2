ed830eb247494a318889dfb0afc74be13acb4cf4
JinyDev <wonjiny.dev@gmail.com>
2024-07-03 16:52:50 +0900

    feat: 외래키 유니크 제약조건으로 인한 게시글 작성 및 수정자 중복 불가 문제 v0.2

--- src/main/java/com/unitalk/program/model/entity/Program.java
+++ src/main/java/com/unitalk/program/model/entity/Program.java
@@ -23,8 +23,8 @@ public class Program {
     private Integer programId; // 집단상담 번호(PK)
 
     @ManyToOne//(fetch = FetchType.LAZY)
-    @JoinColumn(name = "counselorCode", referencedColumnName = "empCode", unique = false)
-    // User 테이블에서 유니크가 설정되어 있기 때문에 Program 테이블에서 유니크 삭제
+    @JoinColumn(unique = false, name = "counselorCode", referencedColumnName = "empCode", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    // foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT) 외래키 삭제
     private Employee counselorCode; // 상담사 코드(FK)
 
     @Column(nullable = false)
