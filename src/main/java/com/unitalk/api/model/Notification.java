7c015bde667901320cd0e9c442c4e5e2fd0d2e4e
HeegeneChae <heegenechae@gmail.com>
2024-07-09 03:25:14 +0900

    feat: Passed API tests for 1:1 chat and notification

---
+++ src/main/java/com/unitalk/api/model/Notification.java
@@ -0,0 +1,14 @@
+package com.unitalk.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    private Long id;
+    private String message;
+    private boolean read;
+}
---
+++ src/main/java/com/unitalk/api/service/ChatService.java
@@ -0,0 +1,22 @@
+package com.unitalk.api.service;
+
+import com.unitalk.api.model.Chat;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChatService {
+    public List<Chat> getChats() {
+        List<Chat> chats = new ArrayList<>();
+        chats.add(new Chat(1L, "안녕하세요!", "user1", LocalDateTime.now()));
+        chats.add(new Chat(2L, "반갑습니다!", "user2", LocalDateTime.now()));
+        return chats;
+    }
+
+    public Chat sendMessage(String message, String sender) {
+        return new Chat(3L, message, sender, LocalDateTime.now());
+    }
+}
