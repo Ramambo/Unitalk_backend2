19bdd8b8032e508de83d371a964cca9c9dae4874
Rbcream <khj0633@gmail.com>
2024-07-16 14:03:57 +0900

    Revert "Reapply "Reapply "Merge branch 'feature/notification' into develop"""
    This reverts commit 4b21f4ca2fd16d8e3a7027943fa064c015bf92e1.

--- src/main/java/com/unitalk/common/config/WebConfig.java
+++ src/main/java/com/unitalk/common/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "ws://localhost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
