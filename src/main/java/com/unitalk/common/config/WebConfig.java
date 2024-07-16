d1e1424c66f40292fe794f4f264e6d1fcef67fcf
Rbcream <khj0633@gmail.com>
2024-07-16 14:37:48 +0900

    Update WebConfig.java

--- src/main/java/com/unitalk/common/config/WebConfig.java
+++ src/main/java/com/unitalk/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
-                .allowedOriginPatterns("http://localhost:3000", "ws://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000", "ws://localhost:3000") //웹소캣포함
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
